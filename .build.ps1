c6fced1759a8c28f3a824da902b7820ff9820f73
Dušan Janošík <dusan@djanosik.cz>
2016-11-27 01:12:55 +0100

    Simplifies build script and fixes build configurations.

--- .build.ps1
+++ .build.ps1
@@ -1,5 +1,8 @@
+$artifactsDir = "$PSScriptRoot\artifacts"
+$version = if ($env:APPVEYOR) { $env:APPVEYOR_BUILD_VERSION } else { "1.0.0-pre" }
+
 task Clean {
-    if(Test-Path artifacts) { del artifacts -Recurse -Force }
+    if (Test-Path $artifactsDir) { del $artifactsDir -Recurse -Force }
     del * -Include bin, obj -Recurse -Force
 }
 
@@ -7,32 +10,24 @@ task RestoreDependencies Clean, {
    exec { dotnet restore }
 }
 
-task PackProjects RestoreDependencies, {
-    $version = if($env:APPVEYOR) { $env:APPVEYOR_BUILD_VERSION } else { "1.0.0-pre" }
+task BuildSolution RestoreDependencies, {
+    dir *.sln | %{
+		exec { dotnet build $_.FullName -c Release /p:Version=$version }
+	}
+}
 
-    if(Test-Path src) {
-        dir src -Include *.csproj -Recurse |% {
-            $project = $_.FullName
-            exec { dotnet pack $project -c Release /p:Version=$version }
+task PackProjects BuildSolution, {
+    if (Test-Path src) {
+        dir src -Include *.csproj -Recurse | %{
+            exec { dotnet pack $_.FullName -c Release -o $artifactsDir --no-build /p:Version=$version }
         }
     }
 }
 
-task CopyArtifacts PackProjects, {
-    ni "artifacts" -ItemType Directory -Force
-    
-    if(Test-Path src) {
-        dir src -Include *.nupkg -Recurse |% {
-            copy $_.FullName ("artifacts") -Force
-        }
-    }
-}
-
-task RunTests CopyArtifacts, {
-    if(Test-Path test) {
-       dir test -Include *.csproj -Recurse |% {
-           $project = $_.FullName
-           exec { dotnet test $project -c Release }
+task RunTests PackProjects, {
+    if (Test-Path test) {
+       dir test -Include *.csproj -Recurse | %{
+           exec { dotnet test $_.FullName -c Release }
        }
     }
 }
