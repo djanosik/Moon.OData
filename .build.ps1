febfbdf5a389d581940e994048c34b0caeb44c22
Dušan Janošík <dusan@djanosik.cz>
2017-02-18 20:05:53 +0100

    Migrates projects to latest SDK, cleans up code.

--- .build.ps1
+++ .build.ps1
@@ -1,35 +1,44 @@
 $artifactsDir = "$PSScriptRoot\artifacts"
 $version = if ($env:APPVEYOR) { $env:APPVEYOR_BUILD_VERSION } else { "1.0.0-pre" }
 
+Exit-Build {
+   move Common.props.backup Common.props -Force
+}
+
 task Clean {
     if (Test-Path $artifactsDir) { del $artifactsDir -Recurse -Force }
     del * -Include bin, obj -Recurse -Force
 }
 
-task RestoreDependencies Clean, {
+task UpdateVersion Clean, {
+   copy Common.props Common.props.backup -Force
+   (gc Common.props).replace("1.0.0-ci", $version) | sc Common.props
+}
+
+task RestoreDependencies UpdateVersion, {
    exec { dotnet restore }
 }
 
 task BuildSolution RestoreDependencies, {
     dir *.sln | %{
-        exec { dotnet build $_.FullName -c Release /p:Version=$version }
+        exec { dotnet build $_.FullName -c Release }
     }
 }
 
-task PackProjects BuildSolution, {
-    if (Test-Path src) {
-        dir src -Include *.csproj -Recurse | %{
-            exec { dotnet pack $_.FullName -c Release -o $artifactsDir --no-build /p:Version=$version }
-        }
-    }
-}
-
-task RunTests PackProjects, {
+task RunTests BuildSolution, {
     if (Test-Path test) {
        dir test -Include *.csproj -Recurse | %{
-           exec { dotnet test $_.FullName -c Release }
+           exec { dotnet test $_.FullName -c Release --no-build }
        }
     }
 }
 
-task . RunTests
+task PackSolution RunTests, {
+    if (Test-Path src) {
+        dir src -Include *.csproj -Recurse | %{
+            exec { dotnet pack $_.FullName -c Release -o $artifactsDir --no-build }
+        }
+    }
+}
+
+task . PackSolution
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Moon.OData changelog
 
+3.0.1
+
+- All libraries are targeting .NET Standard.
+
 2.2.6
 
 - Removed unnecessary dependencies.
